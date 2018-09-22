c027d8053b9cca8b0062468af78d62d4898882bc
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 20:08:40 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan13/relational/PBO210117048Latihan13Relational.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program operasi bilangan dengan 
+ * relational.
+
+ */
+public class PBO210117048Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int bil1 = 10;
+       int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil2 >= bil1));
+        System.out.println("a <= b = " + (bil2 <= bil1));
+        
+    }
+    
+}
